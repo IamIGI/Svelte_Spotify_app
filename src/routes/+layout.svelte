800be64cc9b3c588dc6c99f64d3a89f3d5b7f9eb
i.kłusek <J6ZkqwVUn>
2023-10-18 14:21:21 +0200

    SS-05-Logging out user, removing tokens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,16 @@
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
+	import { LogoutButton } from '$components'; //declare alias in svelte.config.ts file
 
 	export let data: LayoutData;
+
+	$: user = data.user;
 </script>
 
-{data.user?.display_name}
+{#if user}
+	<p>{user.display_name}</p>
+	<LogoutButton />
+{/if}
 
 <slot />
