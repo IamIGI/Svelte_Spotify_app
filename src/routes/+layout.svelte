6b8a25345a7b3ae8fbf59bb35cc0ce1704a4d6c0
i.kłusek <J6ZkqwVUn>
2023-10-19 12:03:10 +0200

    SS-07-Creating and styling login page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,29 @@
 	import 'modern-normalize/modern-normalize.css';
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
-	import { LogoutButton } from '$components'; //declare alias in svelte.config.ts file
 
 	export let data: LayoutData;
 
 	$: user = data.user;
 </script>
 
-{#if user}
-	<p>{user.display_name}</p>
-	<LogoutButton />
-{/if}
+<div id="main">
+	<div id="content">
+		<div id="main-content">
+			<slot />
+		</div>
+	</div>
+</div>
 
-<slot />
+<style lang="scss">
+	#main {
+		#content {
+			#main-content {
+				padding: 30px 15px 60px;
+				@include breakpoint.up('md') {
+					padding: 30px 30px 60px;
+				}
+			}
+		}
+	}
+</style>
