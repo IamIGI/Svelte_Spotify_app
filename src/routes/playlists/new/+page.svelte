d6aa11c2bda80fb04ee3c08159b9caa7bf75e1a3
i.kłusek <J6ZkqwVUn>
2023-10-24 14:27:52 +0200

    SS-28-Create_playlist_with_formActions

--- src/routes/playlists/new/+page.svelte
+++ src/routes/playlists/new/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import Button from '$components/Button.svelte';
+	import { PlaylistForm } from '$components';
 	import type { ActionData, PageData } from './$types';
 
 	export let data: PageData;
@@ -8,37 +8,4 @@
 
 <h2>Add a new playlist!</h2>
 
-<form method="POST">
-	<input hidden name="userID" value={data.user?.id} />
-	<div class="field" class:has-error={form?.nameError}>
-		<label for="playlist-name">Name *</label>
-		<input
-			type="text"
-			id="playlist-name"
-			name="name"
-			placeholder="Playlist name"
-			value={form?.name || ''}
-		/>
-		{#if form?.nameError}
-			<p class="error">{form?.nameError}</p>
-		{/if}
-	</div>
-	<div class="field">
-		<label for="playlist-description">Description</label>
-		<input
-			type="text"
-			id="playlist-description"
-			name="description"
-			placeholder="Playlist description"
-			value={form?.description || ''}
-		/>
-	</div>
-
-	{#if form?.apiError}
-		<p class="error">{form.apiError}</p>
-	{/if}
-
-	<div class="submit-button">
-		<Button element="button" type="submit">Create Playlist</Button>
-	</div>
-</form>
+<PlaylistForm userID={data.user?.id} {form} />
